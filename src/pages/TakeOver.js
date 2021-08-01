0b8c57a25e06c6ef51f5207e071052e0c1725bd7
eunhye <kid146@naver.com>
2021-08-01 15:15:54 +0900

    충돌 해결

--- src/pages/TakeOver.js
+++ src/pages/TakeOver.js
@@ -5,8 +5,14 @@ import styled from 'styled-components';
 import PostList from '../components/posts/PostList';
 
 
-const TakeOver = () => {
-    let title = "인수인계"
+const TakeOver = ( {history} ) => {
+    let title = "인수인계";
+    const user = 'manager';
+    //const user = 'not';
+    if (user !== 'manager') {
+        alert('권한이 없는 페이지입니다.');
+        history.push('/main');
+    }
     return (
         <>
             <Header/>
